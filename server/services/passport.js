a8883ce2d6c85f2dca1201e5c18580630e3524e1
danielpdoherty <danielpdoherty@gmail.com>
2017-08-10 16:40:35 -0500

    added mongoose and cookie-session support, routes for current_user and logout

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,15 +1,39 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done)=>{
+	done(null, user.id); //unique mongoID
+});
+
+passport.deserializeUser((id, done)=>{
+	User.findById(id)
+		.then(user => {
+			done(null, user);
+		});
+});
 
 passport.use(new GoogleStrategy({
 	clientID : keys.googleClientID,
-	clientSecrey : keys.googleClientSecret,
+	clientSecret : keys.googleClientSecret,
 	callbackURL : '/auth/google/callback'
 	}, (accessToken, refreshToken, profile, done) => {
-		console.log('accessToken:', accessToken);
-		console.log('refreshToken:', refreshToken);
-		console.log('profile:', profile);
+		User.findOne({ googleId : profile.id })
+			.then((existingUser)=>{
+				if(existingUser){
+					//already exists
+					done(null, existingUser);
+				} else{
+					//no user exists
+					new User({ googleId : profile.id })
+						.save()
+						.then(user => done(null, user));
+				}
+			});
+
 	})
 );
 
